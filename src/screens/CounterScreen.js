8ac9c908d64c6c191e58c8824902483926ca957e
Daniel Choe <daniel.c@pretty.com.au>
2022-01-17 09:01:59 +1100

    17 Jan

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -5,21 +5,24 @@ const CounterScreen = () => {
   // let counter = 0;
   //we pass in the starting value into useState.
   //we cannot change our piece of state directly. We make reference to the
-  //setCounter() function to make changes to our variable.
+  //setCounter() function to make changes to our variable. (setter function)
+  //when a component is rerendered all of its children get rerendered too.
+  // we never directly modify a state variable. React doesn't detect this change!
+  //Only use the 'setter' function.
   const [counter, setCounter] = useState(0);
 
 
   return (
     <View>
-    <Button title="Increase" onPress={() => {
-      setCounter(counter + 1);
-      // console.log(counter);
-    }}/>
-    <Button title="Decrease" onPress={() => {
-      setCounter(counter - 1);
-      // console.log(counter);
-    }}/>
-      <Text>Current Count: {counter}</Text>
+      <Button title="Increase" onPress={() => {
+        setCounter(counter + 1);
+        // console.log(counter);
+      }}/>
+      <Button title="Decrease" onPress={() => {
+        setCounter(counter - 1);
+        // console.log(counter);
+      }}/>
+        <Text>Current Count: {counter}</Text>
     </View>
   );
 };
