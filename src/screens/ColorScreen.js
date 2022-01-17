4b76a8954660e97bc58ce363d7e242e873332078
Daniel Choe <daniel.c@pretty.com.au>
2022-01-18 08:36:58 +1100

    18 Jan

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Button } from 'react-native';
 
 const ColorScreen = () => {
   // const [] = useState([]);
+  const [colors, setColors] = useState([]);
 
   return (
     <View>
-      <Button title="Add a Color" />
+      <Button title="Add a Color"
+        onPress={() => {
+          setColors([...colors, randomRgb]);
+          console.log(colors);
+          // take a copy of everything in the array and add it to the new one specified.
+          // then we are going to add a new entry calling setRgb.
+        }}
+      />
       <View style={{ height: 100, width: 100, backgroundColor: randomRgb() }} />
     </View>
   )
 };
 
+//defining a function for the background color.
 const randomRgb = () => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
