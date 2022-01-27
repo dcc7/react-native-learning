b81a7b0be77388676f6be90f90b51a65f40ccb34
Daniel Choe <daniel.c@pretty.com.au>
2022-01-28 08:35:31 +1100

    28 Jan

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CounterScreen = () => {
@@ -9,20 +9,36 @@ const CounterScreen = () => {
   //when a component is rerendered all of its children get rerendered too.
   // we never directly modify a state variable. React doesn't detect this change!
   //Only use the 'setter' function.
-  const [counter, setCounter] = useState(0);
+  // const [counter, setCounter] = useState(0);
+
+  const reducer = (state, action) => {
+    // state === { count: number }
+    // action === { type: 'increment' || 'decrement', payload: 1 }
+
+    switch (action.type) {
+      case 'increment':
+        return { ...state, count: state.count + action.payload };
+      case 'decrement':
+        if (state.count > 0) {
+        return { ...state, count: state.count - action.payload };
+        }
+      default:
+        return state;
+    }
+  }
+
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
 
 
   return (
     <View>
       <Button title="Increase" onPress={() => {
-        setCounter(counter + 1);
-        // console.log(counter);
+        dispatch({ type: 'increment', payload: 1 })
       }}/>
       <Button title="Decrease" onPress={() => {
-        setCounter(counter - 1);
-        // console.log(counter);
+        dispatch({ type: 'decrement', payload: 1 })
       }}/>
-        <Text>Current Count: {counter}</Text>
+        <Text>Current Count: { state.count }</Text>
     </View>
   );
 };
