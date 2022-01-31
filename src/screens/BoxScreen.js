fb53bc32167e96339afc58dd76c69cda8fe4f64a
Daniel Choe <daniel.c@pretty.com.au>
2022-02-01 08:38:16 +1100

    1 Feb

---
+++ src/screens/BoxScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const BoxScreen = () => {
+  return <View>
+    <Text>Box Screen</Text>
+  </View>
+}
+
+export default BoxScreen;
--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,17 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextInput ,View, Text, StyleSheet } from 'react-native';
 
 const TextScreen = () => {
 
+  const [password, setPassword] = useState('');
+
+
   return (
     <View>
+    <Text>Enter Password:</Text>
       <TextInput
         placeholder="hello"
         style={styles.input}
         autoCapitalize="none"
         autoCorrect={false}
-        value="hi there"
+        value={password}
+        onChangeText={(newValue) => setPassword(newValue)}
       />
+      { password.length < 4 ? <Text>Password must be 4 characters</Text> : null }
     </View>
   );
 };
