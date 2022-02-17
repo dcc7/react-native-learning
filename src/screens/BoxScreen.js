7894c198aaf3bf5590f8d13418fbae1fbf97a80e
Daniel <danielchoe7@gmail.com>
2022-02-17 15:55:58 +1100

    17 Feb

--- src/screens/BoxScreen.js
+++ src/screens/BoxScreen.js
@@ -2,33 +2,40 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
 const BoxScreen = () => {
-  return <View style={styles.viewStyle}>
-    <Text style={styles.textOneStyle}>Child #1</Text>
-    <Text style={styles.textTwoStyle}>Child #2</Text>
-    <Text style={styles.textThreeStyle}>Child #3</Text>
+  return <View style={styles.parentStyle}>
+    <View style={styles.viewOneStyle}/>
+    <View style={styles.viewTwoStyle}/>
+    <View style={styles.viewThreeStyle}/>
+
   </View>
 }
 
 
 const styles = StyleSheet.create({
-  viewStyle: {
+  parentStyle: {
     borderWidth: 3,
     borderColor: 'black',
     height: 200,
-    alignSelf: 'center'
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+    // alignSelf: 'center',
+    // alignItems: 'flex-end'
   },
-  textOneStyle: {
-    borderWidth: 3,
-    borderColor: 'red',
+  viewOneStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'red'
   },
-  textTwoStyle: {
-    borderWidth: 3,
-    borderColor: 'red',
-    alignSelf: 'stretch'
+  viewTwoStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'green',
+    marginTop: 50
   },
-  textThreeStyle: {
-    borderWidth: 3,
-    borderColor: 'red'
+  viewThreeStyle: {
+    height: 50,
+    width: 50,
+    backgroundColor: 'purple'
   }
 })
 
